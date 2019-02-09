b107818c42278236233a8a91011880297ec9392f
Kirill Korzun <kirill.korzun.97@gmail.com>
2019-02-09 19:42:54 +0300

    Added project.

---
+++ WorkshopHM.Day4.Debugging/DebuggingLibrary/Register.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebuggingLibrary
+{
+    public class Register
+    {
+        List<int> data = new List<int>()
+        {
+            123123,
+            321312,
+            223322
+        };
+
+        public void Enter(string username, int activationKey)
+        {
+            if (username is null)
+            {
+                throw new ArgumentNullException(nameof(username));
+            }
+
+            if (activationKey <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(activationKey));
+            }
+
+            if (!CheckUser(username, activationKey))
+            {
+                Console.WriteLine("Invalid activation key!");
+                return;
+            }
+
+            DoSmth(username);
+        }
+
+        public bool CheckUser(string username, int activationKey)
+        {
+            if (data.Contains(activationKey))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void DoSmth(string username)
+        {
+            Console.WriteLine($"Hello, {username}! Our key has been activated. Let's start to work!");
+        }
+    }
+}
---
+++ WorkshopHM.Day4.Debugging/DebuggingTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using DebuggingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Register register = new Register();
+
+            register.Enter(Console.ReadLine(), 22222);
+
+            Console.ReadLine();
+        }
+    }
+}
